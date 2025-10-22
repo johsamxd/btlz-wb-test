47f4cc9e0afc28aa4a5cf51ae1d1813f6f23fe78
johsamxd <hghgaman@gmail.com>
2025-10-22 14:10:35 +0900

    added spreadsheet id

--- src/postgres/seeds/spreadsheets.js
+++ src/postgres/seeds/spreadsheets.js
@@ -1,10 +1,12 @@
+import env from "#config/env/env.js";
+
 /**
  * @param {import("knex").Knex} knex
  * @returns {Promise<void>}
  */
 export async function seed(knex) {
     await knex("spreadsheets")
-        .insert([{ spreadsheet_id: "some_spreadsheet" }])
+        .insert([{ spreadsheet_id: env.SPREADSHEET_ID }])
         .onConflict(["spreadsheet_id"])
         .ignore();
 }
